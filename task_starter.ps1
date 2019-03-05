c4a13aa8a28d16561fd4958fd81b346399f3f093
Gump Yi <Gump.Yi@quest.com>
2019-03-05 13:23:41 +0800

    Update task_starter.ps1 to support Windows server 2019

--- task_starter.ps1
+++ task_starter.ps1
@@ -13,8 +13,19 @@ if(-not (Test-Path C:\setup)){
 }
 if($env:run_as){
     log ("+"*10+"Process scripts at startup. Current user: $env:run_as"+"+"*10)
-    schtasks /delete /tn ps_executor /f
-    schtasks /create /tn ps_executor /ru $env:run_as /rp $env:run_pwd /tr ("PowerShell -STA -NonInteractive -ExecutionPolicy bypass -file $basedir\ps_executer.ps1") /RL HIGHEST /sc once /st 00:00
+    Unregister-ScheduledTask -TaskName "ps_executor" -Confirm:$false
+    $Time = New-ScheduledTaskTrigger -At 00:00 -Once
+    $action = New-ScheduledTaskAction -Execute "PowerShell -STA -NonInteractive -ExecutionPolicy bypass -file $basedir\ps_executer.ps1"
+    while($true){
+        $Error.Clear()
+        Register-ScheduledTask -TaskName "ps_executor" -User $env:run_as -Password $env:run_pwd -Trigger $Time -Action $action -RunLevel Highest
+        if($Error.Count -eq 0){
+            Break
+        }else{
+            log("Fail to register related scheduledTask within admin credential, retry it after 60s")
+            sleep 60
+        }
+    }    
 }
 else{
     log ("+"*10+"Process scripts at startup. Current user: System"+"+"*10)
